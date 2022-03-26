041c939e51ffd72e04eb5fffb2cbded8e4124e92
Alan Leinhard <61091228+AlanLeinhard@users.noreply.github.com>
2022-03-26 15:01:36 +0300

    step

---
+++ frontend/src/App.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="app">
+        <post-form            
+            @create="createPost"/>
+        <post-list 
+            :posts="posts"
+        />
+    </div>
+</template>
+
+<script>
+    import PostForm from "@/components/PostForm";
+    import PostList from "@/components/PostList";
+
+    export default{
+        components: {
+                PostList,
+                PostForm
+        },
+        data(){
+            return{
+                posts:[
+                    {id:1, title: 'Title', body: 'desc'},
+                    {id:2, title: 'Title 2', body: 'desc 2'},
+                    {id:3, title: 'Title 2', body: 'desc 3'},
+                ],
+            title: '',
+            body: ''
+            }
+        },
+        methods: {
+            createPost(post){
+                this.posts.push(post);
+            },
+        }
+    }
+</script>
+
+<style> 
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+
+.app {
+    padding: 20px;
+}
+</style>
---
+++ frontend/src/components/PostList.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <h3>Users</h3>
+        <post-item class="post" v-for="post in posts"/>
+            <div><strong>Title</strong> {{ post.title }}</div>
+            <div><strong>Body</strong> {{ post.body }}</div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+            posts:{
+                type: Array,
+                required: true
+            }
+        }
+    }
+</script>
+
+<style>
+
+.post {
+    padding: 15px;
+    border: 2px solid teal;
+    margin: 15px;
+}
+</style>
